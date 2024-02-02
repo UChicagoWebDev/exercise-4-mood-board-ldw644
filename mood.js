ec3fcca3ce433f28541d5d01ad9d400bbe1cdb4a
ldw <williamliu711@163.com>
2024-02-02 16:52:29 -0600

    final

--- mood.js
+++ mood.js
@@ -6,11 +6,20 @@ function runSearch() {
   // TODO: Clear the results pane before you run a new search
 
   openResultsPane();
-
+  
+  var inputValue = document.getElementById("inputValue").value;
+  document.getElementById("resultsImageContainer").innerHTML = '';
+  var bing_query = bing_api_endpoint + "?q=" + inputValue;
   // TODO: Build your query by combining the bing_api_endpoint and a query attribute
   //  named 'q' that takes the value from the search bar input field.
 
   let request = new XMLHttpRequest();
+  request.addEventListener("load", requestListener);
+  request.open("GET", bing_query);
+  request.setRequestHeader("Ocp-Apim-Subscription-Key", bing_api_key);
+  request.responseType = 'json';
+  request.send();
+
 
   // TODO: Construct the request object and add appropriate event listeners to
   // handle responses. See:
@@ -31,6 +40,73 @@ function runSearch() {
   return false;  // Keep this; it keeps the browser from sending the event
                   // further up the DOM chain. Here, we don't want to trigger
                   // the default form submission behavior.
+}
+
+function requestListener(e){
+  var imageList = e.target.response.value;
+
+  var suggestionContainer = document.getElementById("suggestions");
+
+  var suggestionChild = suggestionContainer.querySelector('ul');
+
+  suggestionContainer.removeChild(suggestionChild);
+
+  var suggestionList = e.target.response.relatedSearches;
+
+  var ul = document.createElement("ul");
+  
+  for(let i in suggestionList) {
+    var suggestion = suggestionList[i].displayText;
+    var link = suggestionList[i].webSearchUrl;
+
+    var li = document.createElement("li");
+    li.textContent = suggestion;
+    li.onclick = (function(suggestionCopy) {
+      return function() {
+        newSearch(suggestionCopy);
+      };
+    })(suggestion);
+    ul.appendChild(li);
+  }
+
+  suggestionContainer.appendChild(ul);
+
+  for(let i in imageList) {
+    if (i>17) break;
+    var imageSrc = imageList[i].thumbnailUrl;
+    var imageContainer = document.getElementById("resultsImageContainer");
+
+    var div = document.createElement("div");
+    div.className = "resultImage";
+    div.onclick = (function(imageSrcCopy) { 
+      return () => {
+        saveImage(imageSrcCopy);
+      };
+    })(imageSrc);
+
+    var img = document.createElement("img");
+    img.src = imageSrc;
+
+    div.appendChild(img);
+    imageContainer.appendChild(div);
+  }
+
+}
+
+function newSearch(suggestion){
+  document.getElementById("inputValue").value = suggestion;
+  runSearch();
+}
+
+function saveImage(link){
+  div = document.createElement("div");
+  div.className = "savedImage";
+  img = document.createElement("img");
+  img.src = link;
+
+  div.appendChild(img);
+
+  document.getElementById("board").appendChild(div);
 }
 
 function openResultsPane() {
